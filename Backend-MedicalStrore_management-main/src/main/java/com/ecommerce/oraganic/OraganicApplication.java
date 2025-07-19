9d6d394a2af6c50fefa2dc55e19cbf362c5832f9
raghulsolaimalai <125336985+raghulsolaimalai@users.noreply.github.com>
2025-07-19 18:04:20 +0530

    Add files via upload

---
+++ Backend-MedicalStrore_management-main/src/main/java/com/ecommerce/oraganic/OraganicApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.oraganic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OraganicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OraganicApplication.class, args);
+	}
+
+}
---
+++ Backend-MedicalStrore_management-main/src/test/java/com/ecommerce/oraganic/OraganicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.oraganic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OraganicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
